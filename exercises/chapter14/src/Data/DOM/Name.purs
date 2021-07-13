8a521ee208ce8481679f440aaa28a68e1587700e
M-Mafeni <mafeni99@gmail.com>
2021-07-13 23:18:15 +0100

    Chapter 14: Add isMobile

--- exercises/chapter14/src/Data/DOM/Name.purs
+++ exercises/chapter14/src/Data/DOM/Name.purs
@@ -18,6 +18,7 @@ module Data.DOM.Name
   , width
   , height
   , name
+  , isMobile
 
   , attribute, (:=)
   , text
@@ -25,12 +26,14 @@ module Data.DOM.Name
 
   , render
   , testVal
+  , testVal1
   , testVal2
   ) where
 
 import Prelude
 
 import Control.Monad.Free (Free, liftF, runFreeM)
+import Control.Monad.Reader (ReaderT, ask, runReaderT)
 import Control.Monad.State (State, evalState)
 import Control.Monad.State.Trans (put, get)
 import Control.Monad.Writer.Trans (WriterT, execWriterT, tell)
@@ -49,11 +52,13 @@ data ContentF a
   = TextContent String a
   | ElementContent Element a
   | NewName (Name -> a)
+  | Mobile (Boolean -> a)
 
 instance functorContentF :: Functor ContentF where
   map f (TextContent s x) = TextContent s (f x)
   map f (ElementContent e x) = ElementContent e (f x)
   map f (NewName k) = NewName (f <<< k)
+  map f (Mobile k) = Mobile (f <<< k)
 
 newtype Content a = Content (Free ContentF a)
 
@@ -97,6 +102,9 @@ elem e = Content $ liftF $ ElementContent e unit
 
 newName :: Content Name
 newName = Content $ liftF $ NewName identity
+
+isMobile :: Content Boolean
+isMobile = Content $ liftF $ Mobile identity
 
 class IsValue a where
   toValue :: a -> String
@@ -153,12 +161,11 @@ width = AttributeKey "width"
 height :: AttributeKey Int
 height = AttributeKey "height"
 
-type Interp = WriterT String (State Int)
-
+type Interp = ReaderT (Boolean) (WriterT String (State Int))
 -- Content Unit = Content (Free ContentF Unit)
 -- ContentF (Content a) -> Interp (Content a)
 render :: Content Unit -> String
-render (Content c) = evalState (execWriterT $ runFreeM renderContentItem c) 0 where
+render (Content c) = evalState (execWriterT (runReaderT (runFreeM renderContentItem c) false)) 0 where
   renderContentItem :: forall a. ContentF (Free ContentF a) -> Interp (Free ContentF a)
   renderContentItem (TextContent s rest) = do
     tell s
@@ -171,6 +178,9 @@ render (Content c) = evalState (execWriterT $ runFreeM renderContentItem c) 0 wh
     let fresh = Name $ "name" <> show n
     put $ n + 1
     pure (k fresh)
+  renderContentItem (Mobile f) = do
+    onMobile <- ask
+    pure (f onMobile)
   
   renderElement :: Element -> Interp Unit
   renderElement (Element e) = do
@@ -197,15 +207,23 @@ render (Content c) = evalState (execWriterT $ runFreeM renderContentItem c) 0 wh
         tell  e.name
         tell ">"
 
+testVal :: String
 testVal = render $ p [] $ do
   img [ src := "cat.jpg" ]
   text "A cat"
   img [ src := "dog.jpg" ]
 
-
-testVal2 = render $ p [ ] $ do
+testVal1 :: String
+testVal1 = render $ p [ ] $ do
    top <- newName
    a [ name := top ] $
      text "Top"
    a [ href := AnchorHref top ] $
      text "Back to top"
+
+testVal2 :: String
+testVal2 = render $ p [ ] $ do
+  onMobile <- isMobile
+  if onMobile
+    then p [] $ text "I'm on a mobile device"
+    else p [] $ text "I'm on a laptop"
